7f568ee6ae0183c3ebb7d84b97c3f3a5124d8467
Yadi Nugraha <50861102+yadinugrahaa@users.noreply.github.com>
2021-12-12 22:49:29 +0700

    Update vaccine.blade.php

--- MODUL5 YADI/resources/views/vaccine.blade.php
+++ MODUL5 YADI/resources/views/vaccine.blade.php
@@ -104,7 +104,9 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">EDIT VAKSIN</h5>
+
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    
                 </div>
 
                 <form action="/vaccine/update/{{$vaccine->id}}" method="post">
